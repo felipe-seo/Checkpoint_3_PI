9b35d8c35d8df9a4a0be933aa8fbf6ce1b4319b9
Felipe <felipe.seo@fatec.sp.gov.br>
2021-09-30 18:14:00 -0300

    alinhando com o enunciado

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 //Status false significa produto indisponível.
-const produtos = require('./produtos.js')
+let produtos = [
+    {
+        "Nome Do Produto":"Skate XPTO",
+        "Valor Do Produto":1000,
+        "Qualidade Do Produto":8,
+        "Status":true
+    },
+    {
+        "Nome Do Produto":"Jabulani oficial",
+        "Valor Do Produto":600,
+        "Qualidade Do Produto":6,
+        "Status":false
+    },
+    {
+        "Nome Do Produto":"Master tazo veterano",
+        "Valor Do Produto":250,
+        "Qualidade Do Produto":4,
+        "Status":true
+    },
+    {
+        "Nome Do Produto":"Patins Deluxe",
+        "Valor Do Produto":1350,
+        "Qualidade Do Produto":7,
+        "Status":true
+    },
+]
 
 //Função utilizada para atender as condições dadas no problema.
 function filtrar(arr){
