fca137edceec20f163364800a617607a4c8e301f
Felipe <felipe.seo@fatec.sp.gov.br>
2021-09-30 18:11:21 -0300

    table e arquivo de produtos separados

--- script.js
+++ script.js
@@ -1,31 +1,5 @@
 //Status false significa produto indisponível.
-let produtos = [
-    {
-        "Nome Do Produto":"Skate XPTO",
-        "Valor Do Produto":1000,
-        "Qualidade Do Produto":8,
-        "Status":true
-    },
-    {
-        "Nome Do Produto":"Jabulani oficial",
-        "Valor Do Produto":600,
-        "Qualidade Do Produto":6,
-        "Status":false
-    },
-    {
-        "Nome Do Produto":"Master tazo veterano",
-        "Valor Do Produto":250,
-        "Qualidade Do Produto":4,
-        "Status":true
-    },
-    {
-        "Nome Do Produto":"Patins Deluxe",
-        "Valor Do Produto":1350,
-        "Qualidade Do Produto":7,
-        "Status":true
-    },
-]
-
+const produtos = require('./produtos.js')
 
 //Função utilizada para atender as condições dadas no problema.
 function filtrar(arr){
@@ -44,7 +18,7 @@ let carrinho = filtrar(produtos)
 let total = carrinho.reduce((previous, current) => previous["Valor Do Produto"] + current["Valor Do Produto"])
 
 //Opção de exibição 1 - todos os dados dos produtos no carrinho
-console.log(carrinho)
+console.table(carrinho)
 console.log("TOTAL: "+total)
 
 //Trabalha melhor a saída
